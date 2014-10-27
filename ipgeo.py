60846d9ba1a7df96afbfd1c6fd3b9c977d03c84c
Yuri M <ym26@nyu.edu>
2014-10-27 16:32:02 -0400

    added main() function

--- ipgeo.py
+++ ipgeo.py
@@ -13,50 +13,76 @@ import subprocess
 import re
 import time
 
- 
 
  
-# Loop over each line in a open file
-for f_lines in open('sites','r').readlines():
-  f_lines = f_lines.replace("http://","")	# remove http:// from url 
-  f_lines = f_lines.replace("https://","")	# remove https:// from url 
-  f_lines = f_lines.replace("www.","")		# remove www. from url
-  f_lines = (f_lines).split('/')[0]			# remove everything after domain
-  print f_lines 
-  
-  HOST = f_lines.rstrip()					# strip '\n' from each line
-  print "QUERY: %s" % HOST
-  p = subprocess.Popen(['whois %s' % (str(HOST))], shell=True, stdout=subprocess.PIPE)
-  output, err = p.communicate()
-  output = output.split('\n')				# split each line of string output by '\n'
+def WHOIS():
+ 
+  # Loop over each line in a open file
+  for f_lines in open('sites','r').readlines():
+
+    # ISOLATE DOMAIN FROM URL EXTRAS 
+    f_lines = f_lines.replace("http://","")	# remove http:// from url 
+    f_lines = f_lines.replace("https://","")	# remove https:// from url 
+    f_lines = f_lines.replace("www.","")		# remove www. from url
+    f_lines = (f_lines).split('/')[0]			# remove everything after domain
+    f_lines = f_lines.strip('\n')				# remove newline
+    #print f_lines
 
 
 
-  # Extract certain information from whois output
-  info = []
-  for o_lines in (output):
-	if re.search(r"^Domain", o_lines):		# search(pattern,string)
-	  info.append(o_lines)
-	if re.search(r"^Updated", o_lines):
-	  info.append(o_lines)
-	if re.search(r"^Created", o_lines):
-	  info.append(o_lines)
-	if re.search(r"^Admin", o_lines):
-	  info.append(o_lines)
-	if re.search(r"^Registrar", o_lines):
-	  info.append(o_lines)
-	if re.search(r"^Registrant", o_lines):
-	  info.append(o_lines)
-	if re.search(r"^Tech", o_lines):
-	  info.append(o_lines)
-	if re.search(r"^Name", o_lines):
-	  info.append(o_lines)
+    # GET IP FROM HOSTNAME 
+    IP = socket.gethostbyname(f_lines)		# get hostname ip address
+    #print IP
 
 
 
-  # See what info has been collected from whois output
-  for i_lines in (info):
-  	print i_lines
+    # RUN SHELL COMMAND TO QUERY WHOIS DB 
+    HOST = f_lines
+    print "QUERY: %s : %s" % (HOST,IP)
+    p = subprocess.Popen(['whois %s' % (str(HOST))], shell=True, stdout=subprocess.PIPE)
+    output, err = p.communicate()
+    output = output.split('\n')				# split each line of string output by '\n'
 
-  print "\n------SLEEPING 5 sec-------------\n"
-  time.sleep(5)			# sleep required otherwise new query cant be spawned
+
+
+    # EXTRACT SPECIFIC INFORMATION FROM WHOIS OUTPUT 
+    info = []
+    for o_lines in (output):
+	  if re.search(r"^Domain", o_lines):		# search(pattern,string)
+	    info.append(o_lines)
+	  if re.search(r"^Updated", o_lines):
+	    info.append(o_lines)
+	  if re.search(r"^Created", o_lines):
+	    info.append(o_lines)
+	  if re.search(r"^Admin", o_lines):
+	    info.append(o_lines)
+	  if re.search(r"^Registrar", o_lines):
+	    info.append(o_lines)
+	  if re.search(r"^Registrant", o_lines):
+	    info.append(o_lines)
+	  if re.search(r"^Tech", o_lines):
+	    info.append(o_lines)
+	  if re.search(r"^Name", o_lines):
+	    info.append(o_lines)
+
+
+
+    # SHOW END RESULT FROM WHOIS OUTPUT
+    for i_lines in (info):
+  	  print i_lines
+
+
+    print "\n------SLEEPING 5 sec-------------\n"
+    time.sleep(5)			# sleep required otherwise new query cant be spawned
+
+
+
+
+def main():
+  WHOIS() 
+
+
+
+if __name__ == '__main__':
+	main()
+
