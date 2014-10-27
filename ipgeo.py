152e2b39b8591894de4679e1c3afb15776c3f95b
Yuri M <ym26@nyu.edu>
2014-10-27 14:34:14 -0400

    Process each url with jwhois

---
+++ ipgeo.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+#
+# This code was tested on RedHat6.5
+# Requirements
+# 	jwhois 		= rpm linux
+#	subprocess	= python module 
+#
+
+
+import socket
+import subprocess
+import re
+import time
+
+ 
+
+ 
+# Loop over each line in a open file
+for f_lines in open('sites','r').readlines():
+''' '''
+ 
+''' 
+  HOST = f_lines.rstrip()					# strip '\n' from each line
+  print "QUERY: %s" % HOST
+  p = subprocess.Popen(['whois %s' % (str(HOST))], shell=True, stdout=subprocess.PIPE)
+  output, err = p.communicate()
+  output = output.split('\n')				# split each line of string output by '\n'
+
+
+
+  # Extract certain information from whois output
+  info = []
+  for o_lines in (output):
+	if re.search(r"^Domain", o_lines):		# search(pattern,string)
+	  info.append(o_lines)
+	if re.search(r"^Updated", o_lines):
+	  info.append(o_lines)
+	if re.search(r"^Created", o_lines):
+	  info.append(o_lines)
+	if re.search(r"^Admin", o_lines):
+	  info.append(o_lines)
+	if re.search(r"^Registrar", o_lines):
+	  info.append(o_lines)
+	if re.search(r"^Registrant", o_lines):
+	  info.append(o_lines)
+	if re.search(r"^Tech", o_lines):
+	  info.append(o_lines)
+	if re.search(r"^Name", o_lines):
+	  info.append(o_lines)
+
+
+
+  # See what info has been collected from whois output
+  for i_lines in (info):
+  	print i_lines
+
+  print "\n------SLEEPING 5 sec-------------\n"
+  time.sleep(5)			# sleep required otherwise new query cant be spawned
+'''
