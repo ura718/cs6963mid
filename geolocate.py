0dbbf6770601e814062384394a76054b133dad14
Yuri M <ym26@nyu.edu>
2014-10-29 16:07:50 -0400

    added additional ip to test

--- geolocate.py
+++ geolocate.py
@@ -8,10 +8,14 @@
 from geoip import geolite2
 
 
-match = geolite2.lookup('173.252.120.6')
-print match.country
-print match.continent
-print match.timezone
-print match.subdivisions
-print match.location
-print match.ip
+data = ['173.252.120.6', '173.194.123.3', '209.172.54.83']
+
+for ip in data:
+	match = geolite2.lookup(ip)
+	print match.ip
+	print match.country
+	print match.continent
+	print match.timezone
+	print match.subdivisions
+	print match.location
+	print '\n'
