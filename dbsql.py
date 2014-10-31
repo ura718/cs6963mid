6669783337eeca252a512ea3f7c1669cb32305be
Yuri M <ym26@nyu.edu>
2014-10-31 14:27:58 -0400

    script creates db, tables, inserts into column, selects

--- dbsql.py
+++ dbsql.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python
 
+#
+# sqlite3 <file.db>
+# .tables 						= list tables
+# .schema <table> 				= get sql schema when creating a table
+# pragma table_info('<table>')	= list columns 
+#
 
 import sqlite3
 
@@ -10,35 +16,49 @@ def sqlDB():
   try:
     conn = sqlite3.connect(DBFILE)
     cursor = conn.cursor()
-    cursor.execute("DROP TABLE IF EXISTS mydata")
-    cursor.execute("CREATE TABLE mydata(Name TEXT)") 
+    cursor.execute("DROP TABLE IF EXISTS mydata")			# Drop table 'mydata'
+    #cursor.execute("CREATE TABLE mydata(DATA char(200))")   # create table 'mydata' with column 'DATA'
+    cursor.execute("CREATE TABLE mydata(DATA text)")   # create table 'mydata' with column 'DATA'
     conn.commit()
+    conn.close()
   except sqlite3.OperationalError, e:
     print "Database already exists"
     pass
 
 
 
-# Add to database
 def addtoDB(i_data):
   DBFILE = 'data.db'
-  print i_data
+
+  # INSERT i_data INTO DB
   try:
     conn = sqlite3.connect(DBFILE)
-    cursor = conn.cursor()
-    cursor.execute("INSERT INTO mydata VALUES(?)", (str(i_data)))
-    #cursor.execute("INSERT INTO mydata VALUES('hello')")
-    print cursor.execute('SELECT * from mydata')
+    conn.execute("INSERT INTO mydata (DATA) VALUES(?)", (i_data,)) # NOTE: Need comma ',' after i_data
     conn.commit()
+    conn.close()
   except:
-    pass
+    raise
 
+
+
+
+  # SELECT DATA FROM DB
+  try:
+    conn = sqlite3.connect(DBFILE)
+    cursor = conn.execute("SELECT * from mydata")
+    for row in cursor:
+      print row
+    conn.close()
+  except:
+    raise
+  
 
 
 
 sqlDB()
 
-domain=['facebook.com', 'google.com']
+domain=['facebook.com : 127.0.0.1', 'google.com']
 for i_data in domain:
+  print i_data
   addtoDB(i_data) 
   
